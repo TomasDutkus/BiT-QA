dff3637914cff482096a79016688b4575c3c762f
Tomas Dutkus <39946082+TomasDutkus@users.noreply.github.com>
2024-10-22 12:07:07 +0300

    Add files via upload

--- nd/01/app.js
+++ nd/01/app.js
@@ -25,7 +25,7 @@ div2Tag.style.width = "50px";
 div2Tag.style.height = "50px";
 div2Tag.style.border = "1px solid red";
 div2Tag.style.backgroundColor = "red";
-div2Tag.style.borderRadius = "100%";
+div2Tag.style.borderRadius = "50%";
 
 // 3
 
@@ -59,7 +59,109 @@ if (A > B) {
 
 // 4
 
+const C = rand(50, 200);
+const D = rand(50, 200);
+const E = rand(50, 200);
+
+document.querySelector("section#go4").style.display = "flex";
+
+const cTag = document.querySelector("section#go4 div:nth-of-type(1)");
+cTag.style.width = C + "px";
+cTag.style.height = C + "px";
+cTag.style.border = "1px solid blue";
+cTag.style.backgroundColor = "blue";
+cTag.style.borderRadius = "50%";
+
+const dTag = document.querySelector("section#go4 div:nth-of-type(2)");
+dTag.style.width = D + "px";
+dTag.style.height = D + "px";
+dTag.style.border = "1px solid red";
+dTag.style.backgroundColor = "red";
+dTag.style.borderRadius = "50%";
+
+const eTag = document.querySelector("section#go4 div:nth-of-type(3)");
+eTag.style.width = E + "px";
+eTag.style.height = E + "px";
+eTag.style.border = "1px solid green";
+eTag.style.backgroundColor = "green";
+eTag.style.borderRadius = "50%";
+
+cTag.style.order = C;
+dTag.style.order = D;
+eTag.style.order = E;
+
+// 5
+
+const F = rand(-10, 10);
+const G = rand(-10, 10);
+const H = rand(-10, 10);
+
+document.querySelector("section#go5 span:nth-of-type(1)").innerText = F;
+document.querySelector("section#go5 span:nth-of-type(2)").innerText = G;
+document.querySelector("section#go5 span:nth-of-type(3)").innerText = H;
+
+if (F < 0) {
+  document.querySelector("section#go5 span:nth-of-type(1)").style.color = "red";
+} else if (F > 0) {
+  document.querySelector("section#go5 span:nth-of-type(1)").style.color =
+    "blue";
+} else {
+  document.querySelector("section#go5 span:nth-of-type(1)").style.color =
+    "green";
+}
+
+if (G < 0) {
+  document.querySelector("section#go5 span:nth-of-type(2)").style.color = "red";
+} else if (G > 0) {
+  document.querySelector("section#go5 span:nth-of-type(2)").style.color =
+    "blue";
+} else {
+  document.querySelector("section#go5 span:nth-of-type(2)").style.color =
+    "green";
+}
+
+if (H < 0) {
+  document.querySelector("section#go5 span:nth-of-type(3)").style.color = "red";
+} else if (H > 0) {
+  document.querySelector("section#go5 span:nth-of-type(3)").style.color =
+    "blue";
+} else {
+  document.querySelector("section#go5 span:nth-of-type(3)").style.color =
+    "green";
+}
+
 // 6
 
-const span6 = document.querySelector("section#go6 [data-amount] span");
-span6.innerText = 6;
+const zvakes = rand(5, 3000);
+
+let nuolaida;
+
+if (zvakes > 1000 && zvakes < 2000) {
+  nuolaida = "3";
+} else if (zvakes > 2000) {
+  nuolaida = "4";
+} else {
+  nuolaida = "0";
+}
+
+let kaina;
+if (zvakes > 1000 && zvakes < 2000) {
+  kaina = zvakes - zvakes * 0.03;
+} else if (zvakes > 2000) {
+  kaina = zvakes - zvakes * 0.04;
+} else {
+  kaina = zvakes;
+}
+
+const zvakesVnt = document.querySelector("section#go6 [data-amount] span");
+zvakesVnt.innerText = zvakes;
+const zvakesKaina = document.querySelector("section#go6 [data-subtotal] span");
+zvakesKaina.innerText = zvakes;
+const zvakesNuolaida = document.querySelector(
+  "section#go6 [data-discount] span"
+);
+zvakesNuolaida.innerText = nuolaida;
+const galutineKaine = document.querySelector("section#go6 [data-total] span");
+galutineKaine.innerText = kaina;
+
+// 7
