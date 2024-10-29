9d8bf1b8ce5dc3f44e52d27216dc80f42ed4ecbf
Tomas Dutkus <tomasdutkus@gmail.com>
2024-10-29 10:21:23 +0200

    done

--- 13_js/script.js
+++ 13_js/script.js
@@ -13,6 +13,40 @@ const uzduotis8 = document.querySelector("div.select8");
 const uzduotis9 = document.querySelector("div.select9");
 const uzduotis10 = document.querySelector("div.select10");
 
+// Užduotis 1:
+
+const skaicius = rand(-1000, 1000);
+
+if (skaicius > 0) {
+  uzduotis1.innerText = `Skaičius ${skaicius} yra teigiamas`;
+} else if (skaicius < 0) {
+  uzduotis1.innerText = `Skaičius ${skaicius} yra neigiamas`;
+} else {
+  uzduotis1.innerText = `Skaičius yra nulis`;
+}
+
+// Užduotis 2:
+
+const skaicius2 = rand(0, 1000);
+
+if (skaicius2 % 2 == 0) {
+  uzduotis2.innerText = `Skaičius ${skaicius2} yra lyginis`;
+} else {
+  uzduotis2.innerText = `Skaičius ${skaicius2} yra nelyginis`;
+}
+
+// Uzduotis 3:
+
+const temp = rand(-30, 100);
+
+if (temp < 0) {
+  uzduotis3.innerText = `temperatūra ${temp} yra neigiama`;
+} else if (temp >= 0 && temp <= 30) {
+  uzduotis3.innerText = `temperatūra ${temp} yra tarp nulio ir 30`;
+} else {
+  uzduotis3.innerText = `It's getting hot in here`;
+}
+
 // Užduotis 4: Amžiaus tikrinimas
 // Aprašymas: Sukurk kintamąjį amzius ir patikrink, ar amžius yra mažesnis nei 18, tarp 18 ir 65, ar virš 65. Atspausdink rezultatą.
 // Pavyzdys: Jei amzius = 70, konsolėje turėtų atsirasti tekstas „Asmuo yra pensinio amžiaus“.
@@ -25,6 +59,19 @@ if (amzius < 18) {
   uzduotis4.innerText = "Tarp 18 ir 65";
 } else if (amzius > 65) {
   uzduotis4.innerText = "Daugiau nei 65";
+}
+
+// Užduotis 5:
+
+const a = rand(1000, 2000);
+const b = rand(1000, 2000);
+
+if (a > b) {
+  uzduotis5.innerText = `Skaičius ${a} yra didesnis už ${b}`;
+} else if (b > a) {
+  uzduotis5.innerText = `Skaičius ${b} yra didesnis už ${a}`;
+} else {
+  uzduotis5.innerText = `Skaičius ${a} yra lygus ${b}`;
 }
 
 // Užduotis 6: Pasiūlymas pirkėjui
@@ -45,13 +92,17 @@ if (suma < 100) {
 // Aprašymas: Sukurk kintamąjį vardas. Jei vardas yra „Jonas“, atspausdink „Labas, Jonai!“, jei „Ona“ – „Labas, Ona!“, o kitu atveju – „Labas, [vardas]!“.
 // Pavyzdys: Jei vardas = „Ona“, konsolėje turėtų atsirasti tekstas „Labas, Ona!“.
 
-const vardas = "Ona";
+let vardas = rand(1, 4);
+const jonas = 1;
+const ona = 2;
+const tomas = 3;
 
-if (vardas === "Jonas") {
-  uzduotis7.innerText = "Labas, Jonas!";
-} else if (vardas === "Ona") {
+if (vardas === jonas) {
+  uzduotis7.innerText = "Labas, Jonai!";
+} else if (vardas === ona) {
   uzduotis7.innerText = "Labas, Ona!";
-} else {
+} else if (vardas === tomas) {
+  vardas = "Tomas";
   uzduotis7.innerText = `Labas, ${vardas}!`;
 }
 
