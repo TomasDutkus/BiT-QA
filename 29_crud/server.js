1b6d13df4a34865905c0912d44e5080bf831014d
Tomas Dutkus <tomasdutkus@gmail.com>
2024-12-04 13:40:30 +0200

    done

--- 29_crud/server.js
+++ 29_crud/server.js
@@ -2,23 +2,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const handlebars = require("handlebars");
 const fs = require("node:fs");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 const port = 80;
 
-const domain = "http://localhost:80/";
+
+const domain = "http://books.final/";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 const top = fs.readFileSync("./html/top.html", "utf8");
 const bottom = fs.readFileSync("./html/bottom.html", "utf8");
 
+app.get("/", (req, res) => {
+  let books = fs.readFileSync('./data/books.json', 'utf8');
+  books = JSON.parse(books);
+  const file = top + fs.readFileSync("./html/read.html", "utf8") + bottom;
+  const template = handlebars.compile(file);
+  const data = {
+    pageTitle: "Knygu sarasas",
+    domain, books
+  };
+  const html = template(data);
+  res.send(html);
+});
 
 app.get("/create", (req, res) => {
   const file = top + fs.readFileSync("./html/create.html", "utf8") + bottom;
   const template = handlebars.compile(file);
-  const data = { manoKintamasis: "Labas", pageTitle: "Nauja knyga", domain: domain };
+  const data = {
+    manoKintamasis: "Labas",
+    pageTitle: "Nauja knyga",
+    domain: domain,
+  };
   const html = template(data);
   res.send(html);
 });
@@ -26,6 +43,28 @@ app.get("/create", (req, res) => {
 /*const Handlebars = require("handlebars");
 const template = Handlebars.compile("Name: {{name}}");
 console.log(template({ name: "Nils" }));*/
+
+app.post("/store", (req, res) => {
+  const { title, author, year, genre, isbn, pages } = req.body;
+  const id = uuidv4();
+  // need validation
+  const book = {
+    id,
+    title,
+    author,
+    year,
+    genre,
+    isbn,
+    pages,
+  };
+  let data = fs.readFileSync('./data/books.json', 'utf8');
+  data = JSON.parse(data);
+  data.push(book);
+  data = JSON.stringify(data);
+  fs.writeFileSync('./data/books.json', data);
+
+  res.status(302).redirect(domain);
+});
 
 app.listen(port, () => {
   console.log(`Knygynas darbui pasiruoses ant ${port} porto`);
