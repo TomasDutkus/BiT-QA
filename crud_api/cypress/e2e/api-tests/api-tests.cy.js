52a59e6c7c4410a29173a2e9c5804fc72e57b29a
Tomas Dutkus <tomasdutkus@gmail.com>
2025-02-10 10:15:17 +0200

    done

--- crud_api/cypress/e2e/api-tests/api-tests.cy.js
+++ crud_api/cypress/e2e/api-tests/api-tests.cy.js
@@ -1,3 +1,115 @@
-it('Kreipiames i /products', () => {
-    cy.request('GET', 'http://localhost:3000/products')
-});
+// const { response } = require("express");
+
+describe('CRUD_API', () => {
+
+
+
+
+    context('/products', () => {  
+        
+        
+        
+        context('/products atskiri testai', () => {
+            it('/products status kodas 200', ()=> {
+                cy.request("GET", "localhost:3000/products").then((response) => {
+                    expect(response.status).to.be.eq(200);
+                });
+            });
+            it('/products atsakymo laikas', ()=> {
+                cy.request("GET", "localhost:3000/products").then((response) => {
+                    expect(response.duration).to.be.lessThan(1000);
+                });
+            });
+            it('/products netuscias', ()=> {
+                cy.request("GET", "localhost:3000/products").then((response) => {
+                    expect(response.body).length.to.be.greaterThan(1);
+                });
+            });          
+        });    
+        
+        it('/products endpoint bendras testas', () => {
+    
+        cy.request("GET", "localhost:3000/products").then((response) => {
+            //patikrinti status koda
+            expect(response.status).to.be.eq(200);
+            //response
+            expect(response.duration).to.be.lessThan(1000);//1sec
+            expect(response.duration).to.not.be.greaterThan(1000);
+            expect(response.duration).to.be.below(1000);
+    
+            //patikrinti response.body
+            expect(response.body).length.to.be.above(1);
+            //Statusas 200 ir gaunam produktus
+            //{0,1,2 ...}
+            //400 error
+            //{
+            // error: 'kazkas negerai'
+            // }
+            
+            console.log('test');
+            cy.log('test');
+            console.log('pakeitimas')
+    
+            //response.body turi elementa products 
+            // error elemento tikrinimas
+    
+            console.log(response.body);
+            cy.log(response.body);
+            //visi produktai response.body
+    
+    
+            // expect()
+        });
+    
+    });
+    
+    
+    it('/products vieno produkto bendras testas', () => {
+
+        cy.request("GET", "localhost:3000/products/2").then((response) => {
+            expect(response.status).to.be.eq(200);
+            expect(response.body).to.have.property('id', 2);
+            expect(response.body).to.have.property('title', 'test');
+            cy.log(response.body.id);
+            cy.log(response.body.title);
+           // expect(response.body).length.to.be.greaterThan(0);
+            cy.log(response.body);
+    
+        cy.log('pasiruosiau testui')
+    });
+    });
+    it('/products create bendras testas', () => {
+
+        cy.request('POST', 'localhost:3000/products', {
+            title: 'test',
+            description: 'test',
+            price: 100
+        }).then((response) => {
+            expect(response.status).to.be.eq(201);
+            expect(response.body).to.have.property('title', 'test');
+            expect(response.body).to.have.property('description', 'test');
+            expect(response.body).to.have.property('price', 100);
+            cy.log(response.body.title);
+            cy.log(response.body.description);
+            cy.log(response.body.price);
+            cy.log(response.body);
+        });
+    
+        cy.log('pasiruosiau testui')
+    });
+    
+    it('/products update bendras testas', () => {
+    
+        cy.log('pasiruosiau testui')
+    });
+    
+    it('/products delete bendras testas', () => {
+    
+        cy.log('pasiruosiau testui')
+    });
+      });    
+    
+    //users
+    //endpoint n+1
+    
+    });
